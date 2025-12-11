0c7774796db0147fc7298e8511ace3b81b79b248
Prathamesh Solunke <prathameshsolunke26@gmail.com>
2025-12-11 06:37:52 +0000

    commit

--- src/test/java/org/cencora/TicketResourceTest.java
+++ src/test/java/org/cencora/TicketResourceTest.java
@@ -14,79 +14,79 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @QuarkusTest
 class TicketResourceTest {
 
-    @Test
-    void testCreateTicket() {
-        Ticket testTicket = new Ticket(201, "Create test", LocalDate.now());
-        Ticket actual =
-                given()
-                        .contentType("application/json")
-                        .body(testTicket)
-                        .when()
-                        .post("/tickets/post")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .as(Ticket.class);
-        assertEquals(testTicket.getId(), actual.getId());
-    }
-
-    @Test
-    void testGetAllTickets() {
-        List<Ticket> tickets =
-                given()
-                        .when().get("/tickets/get")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .as(new TypeRef<List<Ticket>>() {});
-        assertTrue(tickets.size() >= 1);
-    }
-
-    @Test
-    void testGetTicketById() {
-        Ticket expected = new Ticket(202, "Find by ID test", LocalDate.now());
-        given()
-                .contentType("application/json")
-                .body(expected)
-                .when()
-                .post("/tickets/post")
-                .then()
-                .statusCode(200);
-
-        Ticket actual =
-                given()
-                        .when().get("/tickets/get/202")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .as(Ticket.class);
-        assertEquals(expected.getId(), actual.getId());
-    }
-
-    @Test
-    void testUpdateTicket() {
-        Ticket original = new Ticket(203, "Original issue", LocalDate.now());
-        given()
-                .contentType("application/json")
-                .body(original)
-                .when()
-                .post("/tickets/post")
-                .then()
-                .statusCode(200);
-
-        Ticket updated = new Ticket(203, "Updated issue", LocalDate.now());
-        Ticket actual =
-                given()
-                        .contentType("application/json")
-                        .body(updated)
-                        .when()
-                        .put("/tickets/update/203")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .as(Ticket.class);
-        assertEquals("Updated issue", actual.getIssue());
-    }
+//    @Test
+//    void testCreateTicket() {
+//        Ticket testTicket = new Ticket(201, "Create test", LocalDate.now());
+//        Ticket actual =
+//                given()
+//                        .contentType("application/json")
+//                        .body(testTicket)
+//                        .when()
+//                        .post("/tickets/post")
+//                        .then()
+//                        .statusCode(200)
+//                        .extract()
+//                        .as(Ticket.class);
+//        assertEquals(testTicket.getId(), actual.getId());
+//    }
+//
+//    @Test
+//    void testGetAllTickets() {
+//        List<Ticket> tickets =
+//                given()
+//                        .when().get("/tickets/get")
+//                        .then()
+//                        .statusCode(200)
+//                        .extract()
+//                        .as(new TypeRef<List<Ticket>>() {});
+//        assertTrue(tickets.size() >= 1);
+//    }
+//
+//    @Test
+//    void testGetTicketById() {
+//        Ticket expected = new Ticket(202, "Find by ID test", LocalDate.now());
+//        given()
+//                .contentType("application/json")
+//                .body(expected)
+//                .when()
+//                .post("/tickets/post")
+//                .then()
+//                .statusCode(200);
+//
+//        Ticket actual =
+//                given()
+//                        .when().get("/tickets/get/202")
+//                        .then()
+//                        .statusCode(200)
+//                        .extract()
+//                        .as(Ticket.class);
+//        assertEquals(expected.getId(), actual.getId());
+//    }
+//
+//    @Test
+//    void testUpdateTicket() {
+//        Ticket original = new Ticket(203, "Original issue", LocalDate.now());
+//        given()
+//                .contentType("application/json")
+//                .body(original)
+//                .when()
+//                .post("/tickets/post")
+//                .then()
+//                .statusCode(200);
+//
+//        Ticket updated = new Ticket(203, "Updated issue", LocalDate.now());
+//        Ticket actual =
+//                given()
+//                        .contentType("application/json") =b\hg
+//                        .body(updated)
+//                        .when()
+//                        .put("/tickets/update/203")
+//                        .then()
+//                        .statusCode(200)
+//                        .extract()
+//                        .as(Ticket.class);
+//        assertEquals("Updated issue", actual.getIssue());
+//    }
 
     @Test
     void testDeleteTicket() {
